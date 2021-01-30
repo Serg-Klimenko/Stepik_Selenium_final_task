285058d504870f956aeb94ed029cd6962d6213e3
Serg-Klimenko <klimenko.sm.job@gmail.com>
2021-01-30 17:21:10 +0200

    add catch exeption NoSuchElementException method

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,9 +1,20 @@
+from selenium.common.exceptions import NoSuchElementException
+
 class BasePage():
     # constructor
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     # open page
     def open(self):
         self.browser.get(self.url)
+
+    # catch exeption
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
