65ed37d8e62a47a9643bca892d6a8c3983475e07
Serg-Klimenko <klimenko.sm.job@gmail.com>
2021-01-30 18:50:29 +0200

    modify method should_be_login_page

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,9 +4,6 @@ from selenium.webdriver.common.by import By
 
 
 class LoginPage(BasePage):
-    def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
 
     def should_be_login_page(self):
         self.should_be_login_url()
