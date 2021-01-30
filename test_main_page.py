940e48dd634aab24a749bc777899990ffbcc3c52
Serg-Klimenko <klimenko.sm.job@gmail.com>
2021-01-30 18:31:00 +0200

    add object LoginPage with methods & locators

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,9 @@
-import time
+import pytest
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
+@pytest.mark.skip
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     # init Page Object, pass driver instance and url to the constructor
@@ -10,8 +12,34 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()  # method page - move to the login page
 
 
+@pytest.mark.skip
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_quest_went_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    # init Page Object, pass driver instance and url to the constructor
+    page = LoginPage(browser, link)
+    page.open()  # open page in the browser
+    page.go_to_login_page()  # method page - move to the login page
+    page.should_be_login_url()
+
+
+def test_guest_should_see_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+
+def test_guest_should_see_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
+
+
