8b9193f8e8371201e12894940c69f14592863d26
Serg-Klimenko <klimenko.sm.job@gmail.com>
2021-01-31 19:21:56 +0200

    refactor: unite tests for login in one class

--- test_main_page.py
+++ test_main_page.py
@@ -1,16 +1,25 @@
+import pytest
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    # init Page Object, pass driver instance and url to the constructor
-    page = MainPage(browser, link)
-    page.open()  # open page in the browser
-    page.go_to_login_page()  # method page - move to the login page
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_url()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        # init Page Object, pass driver instance and url to the constructor
+        page = MainPage(browser, link)
+        page.open()  # open page in the browser
+        page.go_to_login_page()  # method page - move to the login page
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_url()
+
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
@@ -21,13 +30,6 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.is_not_product_present()
     basket_page.should_be_message_about_empty_basket()
-
-
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
 
 
 def test_guest_should_see_login_form(browser):
